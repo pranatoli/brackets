9f4e8b1844125b150b55200b0950184c48959e0c
pranatoli <pr.anatoli@gmail.com>
2022-08-04 21:40:53 +0300

    sdfsdf

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    for (const key in bracketsConfig) {
+      if (bracketsConfig[key][1] === (str[i]) && bracketsConfig[key][0] === str[i - 1]) {
+        str = str.slice(0, i - 1) + str.slice(i + 1, str.length);
+        i = 0;
+      }
+    }
+  }
+  return str.length === 0 ? true : false;
 }
